244a98e317d59784c6d3170c66fb0a7d55ae4373
Leonardo Silva Dulci <leonardodulci@gmail.com>
2023-05-11 15:45:21 -0300

    contador-idade

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+
+//repetição do..while
+var c = 1
+
+do {
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 100)
+
+/*
+ repetição while (+ simples e mais encontrada)
+var c = 1
+while(c <= 6){
+    console.log(`passo ${c}`)
+    c++
+}
+
+*/
+
+
+
+
---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,13 @@
+
+function contar(){
+
+var ini = document.querySelector('input#inum')
+var fim = document.querySelector('input#ifim')
+var passo = document.querySelector('input#ipas')
+var box = document.querySelector('div#box')
+var n1 = Number(inum.value)
+var n2 = Number(ifim.value)
+
+box.innerText = ('oi')
+
+}
