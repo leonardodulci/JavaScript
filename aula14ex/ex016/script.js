bb8e7de66a4828c750935d25a65b54a5b24940a3
Leonardo Silva Dulci <leonardodulci@gmail.com>
2023-05-17 10:27:23 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,14 +1,30 @@
+
+function contar(){
 var ini = Number(document.getElementById('inum').value)
 var fim = Number(document.getElementById('ifim').value)
 var passo = Number(document.getElementById('ipas').value)
 var msg = document.getElementById('ires')
-var contador = 0
 
-    function contar(){
-        for(contador = ini ; contador = passo ; contador += passo){
-            msg.innerHTML += `${contador} + `
-        }
-       
+/*
+var ini = 0
+var fim = 10
+var passo = 2
+*/   
+       while(ini <= fim){
+            msg.innerHTML = `${ini}`
+        ini =+ passo
         
-}
+       }
+       
+    }
 
+/*
+var a = 5
+var b = 12
+var c = 2
+
+while(a <= b){
+    console.log(a)
+    a = c+a
+}
+*/
