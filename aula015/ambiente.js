b157eaf610b12923ebc41d9b6d9b4167342f13f6
Leonardo Silva Dulci <leonardodulci@gmail.com>
2023-05-22 22:19:33 -0300

    array

---
+++ aula015/ambiente.js
@@ -0,0 +1,16 @@
+let num = [4, 9, 6, 7]
+
+num.push(8)
+num.sort()
+
+console.log(num)
+console.log(`os vetores são ${num}`)
+console.log(`e o nosso vetor tem ${num.length} posições`)
+
+let pos = num.indexOf(4)
+
+if(pos == -1){
+    console.log('O valor não foi encontrado!!')
+}else {
+    console.log(`o valor está na posição ${pos}`)
+}
