ff5f19e4e1f555c2789c427a3565acc0f503506a
Leonardo Silva Dulci <leonardodulci@gmail.com>
2023-05-26 22:04:57 -0300

    calculador imc com js

--- Praticando/projeto01/script.js
+++ Praticando/projeto01/script.js
@@ -1,4 +1,5 @@
-function calcular(){
+
+    function calcular(){
     var nome = document.getElementById('inome')
     var peso = document.getElementById('ipeso')
     var altura = document.getElementById('ialt')
@@ -17,15 +18,15 @@ function calcular(){
             Você está abaixo do peso!!`
         } 
         
-            else if(imc > 18.6 && imc < 24,9){
+            else if(imc > 18.6 && imc < 24.9){
             res.innerHTML = `Olá, SR.(a) ${n} <br> O seu IMC é ${imc.toFixed(2)} <br> <br>
             Você está no peso ideal, parabéns`
         } 
-                  else if(imc > 25 && imc < 29.9){
+                  else if(imc > 25.0 && imc < 29.9){
             res.innerHTML = `Olá, SR.(a) ${n} <br> O seu IMC é ${imc.toFixed(2)} <br> <br>
             Você está levemente acima do peso!`
         }   
-                    else if(imc > 30.0 && 34.9){
+                    else if(imc > 30.0 && imc < 34.9){
             res.innerHTML = `Olá, SR.(a) ${n} <br> O seu IMC é ${imc.toFixed(2)} <br> <br>
             Obesidade grau I`
         }   
@@ -37,5 +38,5 @@ function calcular(){
             Obesidade grau III (mórbida)`
         }
         }
-    
-             
+
+           
